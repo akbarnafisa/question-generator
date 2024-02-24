10066a266753a09bc76a28fe784b2cbd9725e86c
Akbar Nafisa <akbarnj788@gmail.com>
2024-02-24 09:57:08 +0700

    Add metadata for page title

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -1,5 +1,10 @@
 import { getServerAuthSession } from "~/server/auth";
 import HistoryPage from "./index";
+import { type Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'Questions List - Quiz question generator tool',
+}
 
 export default async function History() {
   const session = await getServerAuthSession();
