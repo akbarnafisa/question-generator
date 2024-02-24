6a9187c346d8ce7eefdaef8c5ef535dff036d263
Akbar Nafisa <akbarnj788@gmail.com>
2024-02-24 09:57:14 +0700

    Add metadata for Questions Page

--- src/app/question/[id]/page.tsx
+++ src/app/question/[id]/page.tsx
@@ -1,5 +1,11 @@
 import { getServerAuthSession } from "~/server/auth";
 import QuestionPage from "./index";
+import { type Metadata } from 'next'
+
+
+export const metadata: Metadata = {
+  title: 'Questions Page - Quiz question generator tool',
+}
 
 export default async function Question({ params }: { params: { id: string } }) {
   const session = await getServerAuthSession();
