cefa55e34063451b4304e2252157cff2f6f98264
Akbar Nafisa <akbarnj788@gmail.com>
2024-03-04 11:21:30 +0700

    Remove unused import and session parameter from History component

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -1,16 +1,14 @@
-import { getServerAuthSession } from "~/server/auth";
 import HistoryPage from "./index";
 import { type Metadata } from 'next'
- 
+
 export const metadata: Metadata = {
   title: 'Questions List - Quiz question generator tool',
 }
 
 export default async function History() {
-  const session = await getServerAuthSession();
   return (
     <>
-      <HistoryPage session={session} />
+      <HistoryPage />
     </>
   );
 }
